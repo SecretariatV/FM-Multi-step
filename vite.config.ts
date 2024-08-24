3160ccca30d91bcc689cde1f7aef001a761cc38c
secretariatv <oliver.b25.f@gmail.com>
2024-08-24 11:51:36 +0900

    create: basic structure

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client"/>
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@assets": "/src/assets",
+      "@features": "/src/features",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+    },
+  },
+  test: {
+    globals: true,
+    css: true,
+    environment: "happy-dom",
+    setupFiles: "./src/setupTest.ts",
+  },
+});
